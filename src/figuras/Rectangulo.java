bf93bfd4f994c2c71b4a8f243d3a6d239b4d2cd0
normins <62967178+normins@users.noreply.github.com>
2021-10-22 09:55:40 -0300

    Se agrega constructor vacío

--- src/figuras/Rectangulo.java
+++ src/figuras/Rectangulo.java
@@ -5,6 +5,9 @@ public class Rectangulo {
 	private double ancho;
 	private double alto;
 
+	public Rectangulo() {
+	}
+
 	public Rectangulo(double ancho, double alto) {
 		this.ancho = ancho;
 		this.alto = alto;
